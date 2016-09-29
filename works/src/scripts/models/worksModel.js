042385c631aae95234a7f9309056226f6ce2205b
黄祯展 <275345465@qq.com>
2016-09-30 00:32:56 +0800

    主页1.2

--- works/src/scripts/models/worksModel.js
+++ works/src/scripts/models/worksModel.js
@@ -6,42 +6,42 @@ var worksModel = [
 },
 {
   'title':'puzzle',
-  'img': 'http://ucren.com/static/locozan/images/puzzle.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/puzzle.png',
   'src': '../src/scripts/writing/puzzle/index.html'
 },
 {
   'title': 'dragInsert',
-  'img': 'http://ucren.com/static/locozan/images/dragInsert.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/dragInsert.png',
   'src': '../src/scripts/writing/dragInsert/index.html'
 },
 {
   'title': 'eg-ipad',
-  'img': 'http://ucren.com/static/locozan/images/ipad.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/ipad.png',
   'src': '../src/scripts/writing/eg-ipad/index.html'
 },
 {
   'title': 'todo',
-  'img': 'http://ucren.com/static/locozan/images/todo.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/todo.png',
   'src': '../src/scripts/writing/todo/index.html'
 },
 {
   'title': 'myalert',
-  'img': 'http://ucren.com/static/locozan/images/myalert.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/myalert.png',
   'src': '../src/scripts/writing/alert/index.html'
 },
 {
   'title':'collision',
-  'img': 'http://ucren.com/static/locozan/images/collision.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/collision.png',
   'src': '../src/scripts/writing/collision/index.html'
 },
 {
   'title': 'knob',
-  'img': 'http://ucren.com/static/locozan/images/knob.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/knob.png',
   'src': '../src/scripts/writing/knob/index.html'
 },
 {
   'title': 'magnifier2',
-  'img': 'http://ucren.com/static/locozan/images/magnifier2.png',
+  'img': 'http://ucren.com/static/locozan/works/src/images/magnifier2.png',
   'src': '../src/scripts/writing/magnifier2/index.html'
 }
 ]
