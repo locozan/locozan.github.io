ac271dd0208653d78759a4594a632a7f14d01e2e
黄祯展 <275345465@qq.com>
2016-10-02 23:54:27 +0800

    作品栏1.4

--- works/src/scripts/models/worksModel.js
+++ works/src/scripts/models/worksModel.js
@@ -2,46 +2,46 @@ var worksModel = [
 {
   'title': 'flappy-bird',
   'img': 'http://ucren.com/static/locozan/images/flappy.png',
-  'src': 'http://ucren.com/static/locozan/flappy-bird/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/flappy-bird/index.html'
 },
 {
   'title':'puzzle',
   'img': 'http://ucren.com/static/locozan/images/puzzle.png',
-  'src': 'http://ucren.com/static/locozan/puzzle/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/puzzle/index.html'
 },
 {
   'title': 'dragInsert',
   'img': 'http://ucren.com/static/locozan/images/dragInsert.png',
-  'src': 'http://ucren.com/static/locozan/dragInsert/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/dragInsert/index.html'
 },
 {
   'title': 'eg-ipad',
   'img': 'http://ucren.com/static/locozan/images/ipad.png',
-  'src': 'http://ucren.com/static/locozan/eg-ipad/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/eg-ipad/index.html'
 },
 {
   'title': 'todo',
   'img': 'http://ucren.com/static/locozan/images/todo.png',
-  'src': 'http://ucren.com/static/locozan/todo/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/todo/index.html'
 },
 {
   'title': 'myalert',
   'img': 'http://ucren.com/static/locozan/images/myalert.png',
-  'src': 'http://ucren.com/static/locozan/alert/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/alert/index.html'
 },
 {
   'title':'collision',
   'img': 'http://ucren.com/static/locozan/images/collision.png',
-  'src': 'http://ucren.com/static/locozan/collision/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/collision/index.html'
 },
 {
   'title': 'knob',
   'img': 'http://ucren.com/static/locozan/images/knob.png',
-  'src': 'http://ucren.com/static/locozan/knob/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/knob/index.html'
 },
 {
   'title': 'magnifier2',
   'img': 'http://ucren.com/static/locozan/images/magnifier2.png',
-  'src': 'http://ucren.com/static/locozan/magnifier2/index.html'
+  'src': 'http://ucren.com/static/locozan/writing/magnifier2/index.html'
 }
 ]
